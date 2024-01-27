993989c4417683a9158d91ed7b6b8fd2a71e198c
Alvin Al <alvvxal@gmail.com>
2024-01-27 12:14:18 +0700

    Editing files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'jost': ["Jost", "sans-serif"],
+      'spacegrotesk': ['"Space Grotesk"', "sans-serif"],
+    },
+  },
+  plugins: [],
+}
+
