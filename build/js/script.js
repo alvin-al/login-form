416faae324b5ce6bf8bd92fb0b82a2643a450cff
Alvin Al Munawwar <alvvxal@gmail.com>
2024-01-28 09:09:37 +0000

    Create successfullogin  notification

--- build/js/script.js
+++ build/js/script.js
@@ -1,18 +1,33 @@
 const wrongAccountDetail = document.getElementById('wrong');
 const wrongCaption = '<p class="text-red-500 mb-2 font-jost font-medium">ⓘ Wrong email or password!</p>';
 
-authenticate = () =>{
+const authenticate = () => {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-
-
-
-    if (email == 'alvin@gmail.com' && password == "123456"){
-        alert('masok');
+    if (email === 'name@gmail.com' && password === '123456') {
+        
+        loginSuccess();
     } else {
-        wrongAccountDetail.innerHTML = wrongCaption;
+        displayWrongCaption();
     }
-
 };
 
+const displayWrongCaption = () => {
+    wrongAccountDetail.innerHTML = wrongCaption;
+};
+
+const loginSuccess = () => {
+    const container = document.getElementById('container');
+
+    container.removeChild();
+
+    const successMessage = document.createElement('div');
+    successMessage.id = 'login-success';
+    successMessage.className = 'p-5 lg:absolute lg:bg-white lg:p-12 lg:right-1 lg:translate-x-[-25%] lg:top-1 lg:translate-y-[25%]';
+    successMessage.innerHTML = `
+        <img src="build/img/success.png" alt="" class="w-24 h-24 m-auto">
+        <p class="font-jost text-lg mb-24 m-auto">Your login successful!</p>`;
+
+    container.appendChild(successMessage);
+};
