1517f1a54fb72688e5c2ae60a386ce13e046e8b1
Alvin Al <alvvxal@gmail.com>
2024-01-29 20:11:54 +0700

    Add success modal when correctly input email and password

--- build/js/script.js
+++ build/js/script.js
@@ -1,33 +1,39 @@
 const wrongAccountDetail = document.getElementById('wrong');
-const wrongCaption = '<p class="text-red-500 mb-2 font-jost font-medium">ⓘ Wrong email or password!</p>';
+const wrongCaption = `<p class="text-red-500 mb-2 font-jost font-medium">ⓘ Wrong email or password!</p>`;
 
 const authenticate = () => {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    if (email === 'name@gmail.com' && password === '123456') {
-        
+    
+
+    if (email === 'name@gmail.com' && password === '1') {
+
         loginSuccess();
     } else {
+        event.preventDefault();
         displayWrongCaption();
     }
 };
+
 
 const displayWrongCaption = () => {
     wrongAccountDetail.innerHTML = wrongCaption;
 };
 
+
+
+
 const loginSuccess = () => {
+
+    event.preventDefault();
     const container = document.getElementById('container');
+    const container2 = document.getElementById('container2');
 
-    container.removeChild();
+    container.innerHTML = '';
+    container2.style.display = 'flex';
 
-    const successMessage = document.createElement('div');
-    successMessage.id = 'login-success';
-    successMessage.className = 'p-5 lg:absolute lg:bg-white lg:p-12 lg:right-1 lg:translate-x-[-25%] lg:top-1 lg:translate-y-[25%]';
-    successMessage.innerHTML = `
-        <img src="build/img/success.png" alt="" class="w-24 h-24 m-auto">
-        <p class="font-jost text-lg mb-24 m-auto">Your login successful!</p>`;
+ 
 
-    container.appendChild(successMessage);
+
 };
